131ee43e1102548ebc5d715dd916b3398fb07f3d
htwang7 <htwang7@gmail.com>
2021-10-03 20:09:43 -0400

    pyvenv

---
+++ pylox/pylox.py
@@ -0,0 +1,7 @@
+
+
+def main() -> None:
+    print ("Start pylox...")
+
+if __name__ == "__main__":
+    main()
---
+++ pylox/pylox.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR
+
+python3 -m venv .venv
+source .venv/bin/activate
+
+python3 pylox.py
