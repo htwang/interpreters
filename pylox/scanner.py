ae18a73d4cc993d5317b9000ff0def7f6a94e5f9
HT Wang <htwang7@gmail.com>
2021-10-04 17:50:10 -0400

    [SCANNER] Complete scanner implementation (#4)

--- pylox/scanner.py
+++ pylox/scanner.py
@@ -1,9 +1,28 @@
 from lox_token import Token
 from token_type import TokenType
-from typing import List, Any
+from typing import List, Any, Optional
 from error import error
 
 class Scanner:
+    
+    KEY_WORDS = {
+        "and": TokenType.AND,
+        "class": TokenType.CLASS,
+        "else": TokenType.ELSE,
+        "false": TokenType.FALSE,
+        "fun": TokenType.FUN,
+        "for": TokenType.FOR,
+        "if": TokenType.IF,
+        "nil": TokenType.NIL,
+        "or": TokenType.OR,
+        "print": TokenType.PRINT,
+        "return": TokenType.RETURN,
+        "super": TokenType.SUPER,
+        "this": TokenType.THIS,
+        "true": TokenType.TRUE,
+        "var": TokenType.VAR,
+        "while": TokenType.WHILE,
+    }
 
     def __init__(self, source_code: str) -> None:
         self._source_code = source_code
@@ -20,7 +39,8 @@ class Scanner:
             # we are at the beginning of the next lexeme
             self._start = self._current
             self._scan()
-
+        
+        self._add_eof_token()
         return self._tokens
     
     def _scan(self) -> None:
@@ -45,6 +65,30 @@ class Scanner:
             self._add_token(TokenType.SEMICOLON)
         elif c == "*":
             self._add_token(TokenType.STAR)
+        elif c == "!":
+            self._add_token(TokenType.BANG_EQUAL if self._match("=") else TokenType.BANG)
+        elif c == "=":
+            self._add_token(TokenType.EQUAL_EQUAL if self._match("=") else TokenType.EQUAL)
+        elif c == ">":
+            self._add_token(TokenType.GREATER_EQUAL if self._match("=") else TokenType.GREATER)
+        elif c == "<":
+            self._add_token(TokenType.LESS_EQUAL if self._match("=") else TokenType.LESS)
+        elif c == "/":
+            if self._match("/"):
+                while self._peek() != "\n" and not self._is_at_end():
+                    self._advance()
+            else:
+                self._add_token(TokenType.SLASH)
+        elif c == '"':
+            self._string()
+        elif c.isdigit():
+            self._number()
+        elif self._isalpha_or_underscore(c):
+            self._identifier()
+        elif c == "\n":
+            self._line += 1
+        elif c in [" ", "\r", "\t"]:
+            return
         else:
             error(self._line, f"Unexpected charactor {c}.")
 
@@ -56,9 +100,88 @@ class Scanner:
         self._current += 1
         return c
 
+    def _match(self, expected: str) -> bool:
+        if self._is_at_end():
+            return False
+        c = self._source_code[self._current]
+        if c == expected:
+            self._current += 1
+            return True
+        return False
+
+    def _peek(self) -> Optional[str]:
+        if self._is_at_end():
+            return None
+        return self._source_code[self._current]
+    
+    def _peek_next(self) -> Optional[str]:
+        if self._current + 1 >= len(self._source_code):
+            return None
+        return self._source_code[self._current + 1]
+
+    @staticmethod
+    def _isalpha_or_underscore(c: str) -> bool:
+        return c == "_" or c.isalpha()
+
+    @staticmethod
+    def _isalnum_or_underscore(c: str) -> bool:
+        return c == "_" or c.isalnum()
+
+    def _string(self) -> None:
+        while self._peek() != '"' and not self._is_at_end():
+            c = self._advance()
+            if c == "\n":
+                self._line += 1
+
+        if self._is_at_end():
+            error(self._line, "Untermindated string.")
+        
+        # consume closing "
+        self._advance()
+
+        # strip off the quotes
+        literal = self._source_code[self._start + 1:self._current - 1]
+        self._add_token_with_literal(TokenType.STRING, literal)
+
+    def _number(self) -> None:
+        c = self._peek()
+        while c is not None and c.isdigit():
+            self._advance()
+            c = self._peek()
+        
+        c = self._peek()
+        cx = self._peek_next()
+
+        if c == "." and cx is not None and cx.isdigit():
+            # consume the dot
+            self._advance()
+
+            c = self._peek()
+            while c is not None and c.isdigit():
+                self._advance()
+                c = self._peek()
+        
+
+        literal = float(self._source_code[self._start: self._current])
+        self._add_token_with_literal(TokenType.NUMBER, literal)
+        
+    def _identifier(self) -> None:
+        c = self._peek()
+        while c is not None and self._isalnum_or_underscore(c):
+            self._advance()
+            c = self._peek()
+        
+        lexeme = self._source_code[self._start:self._current]
+        token_type = self.KEY_WORDS.get(lexeme) or TokenType.IDENTIFIER
+        self._add_token(token_type)
+
     def _add_token(self, ttype: TokenType) -> None:
         self._add_token_with_literal(ttype, None)
 
     def _add_token_with_literal(self, ttype: TokenType, literal: Any) -> None:
         lexeme = self._source_code[self._start:self._current]
         self._tokens.append(Token(ttype=ttype, lexeme=lexeme, literal=literal, line=self._line))
+
+    def _add_eof_token(self) -> None:
+        self._tokens.append(Token(ttype=TokenType.EOF, lexeme="", literal=None, line=self._line))
+
