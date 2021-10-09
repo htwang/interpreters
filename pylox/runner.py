3b6b8d0ca7d116a36eb613e35b5a3c36c5a74f14
htwang7 <htwang7@gmail.com>
2021-10-08 23:18:56 -0400

    init

--- pylox/runner.py
+++ pylox/runner.py
@@ -1,13 +1,17 @@
 """ entry point of the python implementation of the lox language """
 
+from parser import Parser
+
+from pretty_printer import pprint_expr
 from scanner import Scanner
 
 
 def _run(source_code: str) -> None:
     scanner = Scanner(source_code)
     tokens = scanner.scan_tokens()
-    for t in tokens:
-        print(t)
+    parser = Parser(tokens)
+    expr = parser.parse()
+    print(pprint_expr(expr))
 
 
 def run_from_file(file_name: str) -> None:
