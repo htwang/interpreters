c8db17f4655bc86f41111ec3cbdb76de1e745f86
HT Wang <htwang7@gmail.com>
2021-10-10 15:31:04 -0400

    [INTERPRETER] Basic type checking

--- pylox/interpreter.py
+++ pylox/interpreter.py
@@ -1,7 +1,16 @@
+from dataclasses import dataclass
 from typing import Any
 
+from error import error
 from expr import BinaryExpr, Expr, ExprVisitor, GroupExpr, LiteralExpr, UnaryExpr
+from lox_token import Token
 from token_type import TokenType
+
+
+@dataclass
+class LoxRuntimeError(Exception):
+    token: Token
+    msg: str
 
 
 class _Interpreter(ExprVisitor):
@@ -16,11 +25,12 @@ class _Interpreter(ExprVisitor):
 
         op_type = expr.op.ttype
         if op_type == TokenType.MINUS:
+            self._require_type(expr.op, float)
             return -1 * right
         elif op_type == TokenType.BANG:
             return not self._truthy(right)
         else:
-            raise RuntimeError(f"Unsupported op {op_type}")
+            raise self._runtime_error(expr.op, f"Unsupported op {op_type}")
 
     def visit_binary(self, expr: BinaryExpr) -> Any:
         left = self._evaluate(expr.left)
@@ -28,27 +38,38 @@ class _Interpreter(ExprVisitor):
 
         op_type = expr.op.ttype
         if op_type == TokenType.STAR:
+            self._require_type(expr.op, float, left, right)
             return left * right
         elif op_type == TokenType.SLASH:
+            self._require_type(expr.op, float, left, right)
             return left / right
         elif op_type == TokenType.MINUS:
+            self._require_type(expr.op, float, left, right)
             return left - right
         elif op_type == TokenType.PLUS:
+            if isinstance(left, float):
+                self._require_type(expr.op, float, right)
+            if isinstance(left, str):
+                self._require_type(expr.op, str, right)
             return left + right
         elif op_type == TokenType.GREATER:
+            self._require_type(expr.op, float, left, right)
             return left > right
         elif op_type == TokenType.GREATER_EQUAL:
+            self._require_type(expr.op, float, left, right)
             return left >= right
         elif op_type == TokenType.LESS:
+            self._require_type(expr.op, float, left, right)
             return left < right
         elif op_type == TokenType.LESS_EQUAL:
+            self._require_type(expr.op, float, left, right)
             return left <= right
         elif op_type == TokenType.EQUAL_EQUAL:
             return left == right
         elif op_type == TokenType.BANG_EQUAL:
             return left != right
         else:
-            raise RuntimeError(f"Unsupported op {op_type}")
+            raise self._runtime_error(expr.op, f"Unsupported op {op_type}")
 
     def _evaluate(self, expr: Expr) -> Any:
         return expr.accept(self)
@@ -58,6 +79,22 @@ class _Interpreter(ExprVisitor):
             return value
         return value is not None
 
+    def _require_type(self, token: Token, expected_type: type, *values: Any) -> None:
+        for value in values:
+            if not isinstance(value, expected_type):
+                raise self._runtime_error(
+                    token,
+                    f"type mismatched for {token.lexeme}, expected type is {expected_type}",
+                )
+
+    @staticmethod
+    def _runtime_error(token: Token, msg: str) -> LoxRuntimeError:
+        return LoxRuntimeError(token=token, msg=msg)
+
 
 def interpret(expr: Expr) -> Any:
-    return expr.accept(_Interpreter())
+    try:
+        return expr.accept(_Interpreter())
+    except LoxRuntimeError as e:
+        error(e.token.line, e.msg)
+        return None
