c6ffad3a3649eb6809948f82d9a4321ba6788128
htwang7 <htwang7@gmail.com>
2021-10-16 22:26:21 -0400

    inc

--- pylox/env.py
+++ pylox/env.py
@@ -14,22 +14,21 @@ class Environment:
 
     def assign(self, token: Token, value: Any) -> None:
         name = token.lexeme
-        if name not in self._map:
-            if self._enclosed is not None:
-                self._enclosed.assign(token, value)
-            else:
-                raise LoxRuntimeError(
-                    token=token, msg=f"Undefined variable {token.lexeme}"
-                )
-        self._map[name] = value
+        if name in self._map:
+            self._map[name] = value
+            return
+
+        if self._enclosed is not None:
+            self._enclosed.assign(token, value)
+        else:
+            raise LoxRuntimeError(token=token, msg=f"Undefined variable {token.lexeme}")
 
     def get_value(self, token: Token) -> Any:
         name = token.lexeme
-        if name not in self._map:
-            if self._enclosed is not None:
-                self._enclosed.get_value(token)
-            else:
-                raise LoxRuntimeError(
-                    token=token, msg=f"Undefined variable {token.lexeme}"
-                )
-        return self._map[name]
+        if name in self._map:
+            return self._map[name]
+
+        if self._enclosed is not None:
+            self._enclosed.get_value(token)
+        else:
+            raise LoxRuntimeError(token=token, msg=f"Undefined variable {token.lexeme}")
