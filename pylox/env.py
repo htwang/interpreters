5857a18cdd45aa03c984dc0e3cf80b5032c136aa
htwang7 <htwang7@gmail.com>
2021-10-16 22:24:25 -0400

    init

--- pylox/env.py
+++ pylox/env.py
@@ -1,12 +1,13 @@
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from error import LoxRuntimeError
 from lox_token import Token
 
 
 class Environment:
-    def __init__(self) -> None:
+    def __init__(self, enclosed: Optional["Environment"] = None) -> None:
         self._map: Dict[str, Any] = {}
+        self._enclosed = enclosed
 
     def define(self, name: str, value: Any) -> None:
         self._map[name] = value
@@ -14,11 +15,21 @@ class Environment:
     def assign(self, token: Token, value: Any) -> None:
         name = token.lexeme
         if name not in self._map:
-            raise LoxRuntimeError(token=token, msg=f"Undefined variable {token.lexeme}")
+            if self._enclosed is not None:
+                self._enclosed.assign(token, value)
+            else:
+                raise LoxRuntimeError(
+                    token=token, msg=f"Undefined variable {token.lexeme}"
+                )
         self._map[name] = value
 
     def get_value(self, token: Token) -> Any:
         name = token.lexeme
-        if name in self._map:
-            return self._map[name]
-        raise LoxRuntimeError(token=token, msg=f"Undefined variable {token.lexeme}")
+        if name not in self._map:
+            if self._enclosed is not None:
+                self._enclosed.get_value(token)
+            else:
+                raise LoxRuntimeError(
+                    token=token, msg=f"Undefined variable {token.lexeme}"
+                )
+        return self._map[name]
